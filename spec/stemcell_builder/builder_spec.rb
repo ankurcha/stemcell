90fd17240444583b88067ed4ce076d136a031314
Ankur Chauhan <ankurc@vmware.com>
2013-02-01 15:01:07 -0800

    More test cleanup

--- spec/stemcell_builder/builder_spec.rb
+++ spec/stemcell_builder/builder_spec.rb
@@ -5,7 +5,10 @@ describe Bosh::Agent::StemCell::BaseBuilder do
   before(:each) do
     @log = Logger.new(STDOUT)
     @log.level = Logger::DEBUG
-    @stemcell = Bosh::Agent::StemCell::BaseBuilder.new({:type => "testing-stemcell", :logger => @log}, {})
+    @prefix_dir = Dir.mktmpdir
+    @agent_file = File.join(@prefix_dir, "bosh-agent.gem")
+    FileUtils.touch @agent_file
+    @stemcell = Bosh::Agent::StemCell::BaseBuilder.new({:type => "noop", :logger => @log, :prefix => @prefix_dir, :agent_src_path => @agent_file}, {})
   end
 
   it "Initializes the stemcell manifest with defaults" do
@@ -75,15 +78,11 @@ describe Bosh::Agent::StemCell::BaseBuilder do
   end
 
   it "Compiles all the erb files as a part of the setup" do
-    prefix_dir = Dir.mktmpdir
-    agent_file = File.join(prefix_dir, "bosh-agent.gem")
-    FileUtils.touch agent_file
-    @stemcell = Bosh::Agent::StemCell::BaseBuilder.new({:type => "noop", :logger => @log, :prefix => prefix_dir, :agent_src_path => agent_file}, {})
-    Dir.chdir(prefix_dir) do
+    Dir.chdir(@prefix_dir) do
       @stemcell.setup
-      filename = File.join(prefix_dir, "definitions", @stemcell.name, "erbtest.txt")
+      filename = File.join(@prefix_dir, "definitions", @stemcell.name, "erbtest.txt")
       File.exists?(filename).should eq true
-      File.read(File.join(prefix_dir, "definitions", @stemcell.name, "erbtest.txt")).should eq @stemcell.name
+      File.read(File.join(@prefix_dir, "definitions", @stemcell.name, "erbtest.txt")).should eq @stemcell.name
     end
   end
 
