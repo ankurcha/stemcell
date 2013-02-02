3052d960a8a090fb82be318b96ca00e9d56b0c55
Ankur Chauhan <ankurc@vmware.com>
2013-02-01 16:28:07 -0800

    Made apt-get update verbose for some peace of mind while installing

--- templates/ubuntu/apt-upgrade.sh
+++ templates/ubuntu/apt-upgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Upgrade all packages
-apt-get -y -qq update
+apt-get -y update
 
-apt-get -y -qq --force-yes install build-essential libssl-dev lsof \
+apt-get -y --force-yes install build-essential libssl-dev lsof \
 strace bind9-host dnsutils tcpdump iputils-arping \
 curl wget libcurl3 libcurl3-dev bison libreadline6-dev \
 libxml2 libxml2-dev libxslt1.1 libxslt1-dev zip unzip \
@@ -12,6 +12,6 @@ rsync openssh-server traceroute libncurses5-dev quota \
 libaio1 gdb psmisc dialog bridge-utils debootstrap libcap-dev libyaml-dev
 
 # Upgrade packages to latest version
-apt-get -y -qq update
-apt-get -y -qq upgrade
+apt-get -y update
+apt-get -y upgrade
 apt-get clean
