afadef34bbfe2770f005c71f3649292eee221a7e
Ankur Chauhan <ankur@malloc64.com>
2013-02-05 18:04:50 -0800

    Add dev packages

--- templates/centos/base.sh
+++ templates/centos/base.sh
@@ -15,7 +15,10 @@ enabled=1
 gpgcheck=0
 EOM
 
+# system update
 yum -y update
+yum -y groupinstall "Development Tools"
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel \
 readline-devel sqlite-devel perl wget dkms curl ntp crontabs sysstat
-
+yum -y install libxslt-devel libyaml-devel libxml2-devel gdbm-devel libffi-devel zlib-devel \
+openssl-devel libyaml-devel readline-devel curl-devel openssl-devel pcre-devel git
