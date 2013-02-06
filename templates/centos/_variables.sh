db1367b1973f25bc1d2c4a773929fa0e46a2e19a
Ankur Chauhan <ankur@malloc64.com>
2013-02-05 16:23:09 -0800

    Add SRC_DIR

--- templates/centos/_variables.sh
+++ templates/centos/_variables.sh
@@ -4,6 +4,7 @@ bosh_app_dir=/var/vcap
 bosh_dir=$bosh_app_dir/bosh
 bosh_users_password="c1owdc0w"
 system_parameters_infrastructure="vsphere"
+SRC_DIR=`pwd`
 
 if [ ! -d "$bosh_dir" ]; then
 	# create bosh_dir and add to path
