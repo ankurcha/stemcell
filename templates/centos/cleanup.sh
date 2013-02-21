bdfd4b9c452ccb60d7e860a5d2084e274c9ab609
Ankur Chauhan <ankur@malloc64.com>
2013-02-21 12:09:35 -0800

    fix/upgrade ruby installer, added host_keys cleanup step

--- templates/centos/cleanup.sh
+++ templates/centos/cleanup.sh
@@ -11,3 +11,6 @@ yum -y clean all
 rm -f _60-bosh-sysctl.conf _monitrc _ntpdate _sysstat _empty_state.yml _variables.sh _helpers.sh _bosh_agent.tar
 rm -f base.sh sudo.sh setup-bosh.sh monit.sh ruby.sh bosh_agent.sh vmware-tools.sh harden.sh timestamp.sh postinstall.sh cleanup.sh zerodisk.sh
 rm -f *.iso *.gem
+
+# Clean out ssh host keys
+rm -f /etc/ssh/ssh_host_*
