7ea6347ddddc76c7bd83b8aefd4444f7dcadb955
Ankur Chauhan <ankurc@vmware.com>
2013-02-28 19:40:01 -0800

    Fix cleanup

--- templates/centos/cleanup.sh
+++ templates/centos/cleanup.sh
@@ -7,11 +7,6 @@ source _variables.sh
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 
-# Clean out all the scripts
-rm -f _60-bosh-sysctl.conf _monitrc _ntpdate _sysstat _empty_state.yml _variables.sh _helpers.sh _bosh_agent.tar
-rm -f base.sh sudo.sh setup-bosh.sh monit.sh ruby.sh bosh_agent.sh vmware-tools.sh harden.sh timestamp.sh postinstall.sh cleanup.sh zerodisk.sh
-rm -f *.iso *.gem
-
 # install runonce
 mkdir -p /etc/local/runonce.d/ran
 cp $SRC_DIR/_runonce /usr/local/bin/runonce
@@ -20,5 +15,10 @@ chmod +x /usr/local/bin/runonce
 # Do some firstboot clean up
 # Regenerate ssh keys
 /usr/local/bin/runonce "rm -f /etc/ssh/ssh_host_*"
-ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
-ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+/usr/local/bin/runonce "ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''"
+/usr/local/bin/runonce "ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''"
+
+# Clean out all the scripts
+rm -f _60-bosh-sysctl.conf _monitrc _ntpdate _sysstat _empty_state.yml _variables.sh _helpers.sh _bosh_agent.tar _runonce
+rm -f base.sh sudo.sh setup-bosh.sh monit.sh ruby.sh bosh_agent.sh vmware-tools.sh harden.sh timestamp.sh postinstall.sh cleanup.sh zerodisk.sh
+rm -f *.iso *.gem
