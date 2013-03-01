5e2e2dfbbd9027f7013487983f7725b7f0c845f2
Ankur Chauhan <ankurc@vmware.com>
2013-02-28 16:46:46 -0800

    Use linux-virtual kernel

--- templates/micro/apt-upgrade.sh
+++ templates/micro/apt-upgrade.sh
@@ -4,9 +4,6 @@ source _variables.sh
 # Upgrade all packages
 apt-get -y update
 
-# install linux-virtual kernel
-apt-get -y install linux-virtual
-
 apt-get -y --force-yes install build-essential libssl-dev lsof \
 strace bind9-host dnsutils tcpdump iputils-arping \
 curl wget libcurl3 libcurl3-dev bison libreadline6-dev \
