6fea7109bf8bc730e3439a000859e3b44febbc5e
Ankur Chauhan <ankurc@vmware.com>
2013-03-06 11:20:24 -0800

    Add logging

--- lib/stemcell/builder.rb
+++ lib/stemcell/builder.rb
@@ -133,10 +133,10 @@ module Bosh::Agent::StemCell
     def generate_image
       image_path = File.join @prefix, "image"
       Dir.chdir(@prefix) do
-        sh("tar -xzf #@vm_name.box > /dev/null 2>&1", {:on_error => "Unable to unpack .box file"})
+        sh("tar -xzf #@vm_name.box", {:on_error => "Unable to unpack .box file"})
         vmdk_filename = Dir.glob("*.vmdk").first
         Dir.glob("*.ovf") { |ovf_file| fix_virtualbox_ovf(ovf_file, vmdk_filename) } # Fix ovf files
-        sh("tar -czf #{image_path} *.vmdk *.ovf > /dev/null 2>&1", {:on_error=>"Unable to create image file from ovf and vmdk"})
+        sh("tar -czf #{image_path} *.vmdk *.ovf", {:on_error=>"Unable to create image file from ovf and vmdk"})
         FileUtils.rm [Dir.glob('*.box'), Dir.glob('*.vmdk'), Dir.glob('*.ovf'), "Vagrantfile"]
         @image_sha1 = Digest::SHA1.file(image_path).hexdigest
       end
