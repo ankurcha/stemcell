b20e8947a9816e5ef7130da60fc7a066a06b03af
Ankur Chauhan <ankur@malloc64.com>
2013-03-12 01:42:09 -0700

    add ruby libraries

--- templates/ubuntu/ruby.sh
+++ templates/ubuntu/ruby.sh
@@ -4,7 +4,7 @@ set -x
 source _variables.sh
 
 ### stage bosh_ruby
-apt-get -y install build-essential zlib1g-dev libssl-dev libxml2-dev libxslt-dev libreadline6-dev libyaml-dev libffi-dev libruby1.9
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libcurl4-openssl-dev curl git-core python-software-properties
 
 # install ruby
 pushd /tmp
